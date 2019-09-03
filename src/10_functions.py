99f0517cfe4a695210b16277de440e6c41850cc3
Doug <me@douglasbowen.com>
2019-09-03 18:11:29 -0400

    10

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,11 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even_the_classic(n):
+        if n % 2 == 0:
+            print('even')
+        else:
+            print('odd')
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -9,4 +14,4 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+is_even_the_classic(num)
