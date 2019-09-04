ee8ef2679b851ab34d1e266a3f49d367e8b9cecd
Doug <me@douglasbowen.com>
2019-09-03 23:28:27 -0400

    14

--- src/14_cal.py
+++ src/14_cal.py
@@ -22,3 +22,26 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+
+x = input("Enter comma-separated month and year ").split(',')
+y = [int(x) for x in x]
+
+
+def big_cal(year, m=1) :
+        print ("Calendar of March 2019 is:")
+        print (calendar.month(year, m, 2, 1))
+print(y[1], y[0])
+
+big_cal(y[0], y[1])
+
+"""
+def is_even_the_classic(n):
+        if n % 2 == 0:
+            print('even')
+        else:
+            print('odd')
+
+
+is_even_the_classic(num)
+"""
